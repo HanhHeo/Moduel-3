5899d97064821bf3c36df76ed5f1875549845d4e
hanhheo1806 <hanhnguyen18-695@gmial.com>
2021-11-12 08:43:42 +0700

    Task-Manager

---
+++ task_manager/resources/views/moduels/customer/index.blade.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+<h1>Danh sách khách hàng</h1>
+<table border="1">
+    <thead>
+    <tr>
+        <th>STT</th>
+        <th>Họ và tên</th>
+        <th>Số điện thoại</th>
+        <th>Email</th>
+        <th>Thao tác</th>
+    </tr>
+    </thead>
+    <tbody>
+    <tr>
+        <td>1</td>
+        <td>Nguyễn Sỹ Hạnh</td>
+        <td>0981568889</td>
+        <td>sh.@mail.com</td>
+        <td>
+            <a href="#">Xem</a> | <a href="#">Sửa</a> | <a href="#">Xóa</a>
+        </td>
+    </tr>
+    <tr>
+        <td>2</td>
+        <td>Nguyễn Thế Cường</td>
+        <td>01234567890</td>
+        <td>Cưn@mail.com</td>
+        <td>
+            <a href="#">Xem</a> | <a href="#">Sửa</a> | <a href="#">Xóa</a>
+        </td>
+    </tr>
+    <tr>
+        <td>3</td>
+        <td>Hồ Xuân Cường</td>
+        <td>0374123456</td>
+        <td>cuongphuho@mail.com</td>
+        <td>
+            <a href="#">Xem</a> | <a href="#">Sửa</a> | <a href="#">Xóa</a>
+        </td>
+    </tr>
+    <tr>
+        <td>4</td>
+        <td>Lê Quốc Hưng</td>
+        <td>0222222222</td>
+        <td>email.test@mail.com</td>
+        <td>
+            <a href="#">Xem</a> | <a href="#">Sửa</a> | <a href="#">Xóa</a>
+        </td>
+    </tr>
+    <tr>
+        <td>5</td>
+        <td>Nguyễn Văn E</td>
+        <td>01234567890</td>
+        <td>email.test@mail.com</td>
+        <td>
+            <a href="#">Xem</a> | <a href="#">Sửa</a> | <a href="#">Xóa</a>
+        </td>
+    </tr>
+    </tbody>
+</table>
+</body>
+</html>
